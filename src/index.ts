fdb46cb986630291b93d51ccedeae74a887180b0
JunDevl <juninhoplay.pedone@gmail.com>
2025-09-24 22:42:49 -0300

    Added some functionality

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
 import "./style.css";
 
-console.log("test")
+const details = document.querySelectorAll("details");
+
+const expand = document.querySelector("#expand-toggle");
+
+details.forEach((detailElem) => {
+  detailElem.lastElementChild.addEventListener("click", (e) => detailElem.toggleAttribute("open"));
+})
+
+expand.addEventListener("click", (e) => {
+  const listElement = expand.parentElement;
+  
+  if (listElement.getAttribute("expand") === null) {
+    listElement.style.width = "var(--sidenav-width)"
+    listElement.toggleAttribute("expand");
+    return
+  }
+
+  listElement.toggleAttribute("expand");
+  listElement.style.width = "calc(var(--sidenav-width) + 20rem)"
+})
